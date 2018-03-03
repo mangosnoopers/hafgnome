e13817270fe30c7d01af362c641e20cc3669a797
unknown <jd794@cornell.edu>
2018-03-03 14:11:00 -0500

    Wheel boi

---
+++ code/core/src/edu/cornell/gdiac/mangosnoops/entity/Wheel.java
@@ -0,0 +1,25 @@
+package edu.cornell.gdiac.mangosnoops.entity;
+
+import edu.cornell.gdiac.mangosnoops.*;
+import edu.cornell.gdiac.util.*;
+
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.graphics.*;
+
+public class Wheel {
+
+    // Coordinates of wheel center
+    static private Vector center;
+
+    // Vector from center of wheel to its outer radius
+    static private Vector2 outerRadius;
+
+    // Vector from center of wheel to its inner radius
+    static private Vector2 innerRadius;
+
+    public Wheel(){ }
+
+    public void rotate(float theta){return;}
+
+    public void snapBack(float theta){return;}
+}
