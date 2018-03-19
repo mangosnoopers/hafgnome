7c8265660718de7b76afb5e95b24ec8d5f7bdc6e
stsinwell <sas575@cornell.edu>
2018-03-19 02:22:28 -0400

    putting in jenns assets

--- core/src/edu/cornell/gdiac/mangosnoops/GameCanvas.java
+++ core/src/edu/cornell/gdiac/mangosnoops/GameCanvas.java
@@ -612,12 +612,12 @@ public class GameCanvas {
 
 		camera.position.set(camera.position.x + camOffset, camera.position.y, camera.position.z);
 
-		if (camera.position.x > 0.15f) {
-			camera.position.x = 0.15f;
+		if (camera.position.x > 0.25f) {
+			camera.position.x = 0.25f;
 		}
 
-		if (camera.position.x < -0.15f) {
-			camera.position.x = -0.15f;
+		if (camera.position.x < -0.25f) {
+			camera.position.x = -0.25f;
 		}
 
 		for (Decal d : roadDecals) {
