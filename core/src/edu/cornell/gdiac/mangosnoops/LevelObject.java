3c02621f4b6e3098315b695244867f041dca8830
stsinwell <sas575@cornell.edu>
2018-03-28 14:28:09 -0400

    levelelvelelvlelvel

--- core/src/edu/cornell/gdiac/mangosnoops/LevelObject.java
+++ core/src/edu/cornell/gdiac/mangosnoops/LevelObject.java
@@ -1,39 +1,211 @@
 package edu.cornell.gdiac.mangosnoops;
 
 import com.badlogic.gdx.utils.*;
+import edu.cornell.gdiac.mangosnoops.hudentity.Radio.*;
 import edu.cornell.gdiac.mangosnoops.roadentity.*;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import edu.cornell.gdiac.mangosnoops.GameplayController.*;
+
 
 public class LevelObject {
-    /** All the objects at the beginning of the level. */
-    private Array<Gnome> gnomez;
-    /** Number of Lanes in the level. */
+    /** Region that the level takes place in */
+    private Region region;
+    /** Level speed */
+    private float speed;
+    /** Number of lanes in the level. */
     private int numLanes;
+    /** Total number of blocks defined in the level file */
+    private int totalBlocks;
+    /** Number of blocks to select from the total number of blocks */
+    private int useBlocks;
+    /** Padding between enemies */
+    private float padding;
+    /** Mapping between the level's song genres and its song mp3 files */
+    private ObjectMap<Genre,String> songs;
+    /** Array of enemies for the level */
+    private Array<Gnome> gnomez;
+
+    /** The car */
     private Car yonda;
+    /** Inventory */
+    // TODO
 
-    public int getLanes() { return numLanes; }
+    /** Speed constants */
+    private static final float VERY_SLOW_SPEED = 0.0f;
+    private static final float SLOW_SPEED = 0.0f;
+    private static final float NORMAL_SPEED = 0.0f;
+    private static final float FAST_SPEED = 0.0f;
+    private static final float VERY_FAST_SPEED = 0.0f;
 
-    public Array<Gnome> getGnomez() { return gnomez; }
+    /** Padding constants */
+    private static final float LESS_PADDING = 0.0f;
+    private static final float NORMAL_PADDING = 0.0f;
+    private static final float MORE_PADDING = 0.0f;
 
-    public Car getCar() { return yonda; }
+    /** Excel spreadsheet constants */
+    /** Row and column of the cell containing region information */
+    private static final int REGION_ROW = 1;
+    private static final int REGION_COL = 0;
+    /** Row and column of cell containing speed information */
+    private static final int SPEED_ROW = 1;
+    private static final int SPEED_COL = 1;
+    /** Row and column of the cell containing lane number information */
+    private static final int LANE_ROW = 1;
+    private static final int LANE_COL = 2;
 
-    /** A mapping between the level's song genres and its
-     * song mp3 files
-     */
-    private ObjectMap<String,String> stationGenreMP3Mapping;
+    /** Row and column of cell for total number of blocks */
+    private static final int TOTAL_BLOCKS_ROW = 4;
+    private static final int TOTAL_BLOCKS_COL = 0;
+    /** Row and column of cell for num blocks to use */
+    private static final int USE_BLOCKS_ROW = 4;
+    private static final int USE_BLOCKS_COL = 1;
+    /** Row and column of cell for enemy padding */
+    private static final int PADDING_ROW = 4;
+    private static final int PADDING_COL = 2;
+
+    /** Row and column of cells containing song information */
+    private static final int SONGS_START_ROW = 8;
+    private static final int SONGS_END_ROW = 12;
+    private static final int SONG_GENRE_COL = 0;
+    private static final int SONG_FILE_COL = 1;
+
+    /** Start of road layout data */
+    private static final int ROAD_START_ROW = 1;
+    private static final int ROAD_START_COL = 4;
 
     /**
-     * Loads in JSON File to create a Level Object.
+     * Return the region for this level.
+     */
+    public Region getRegion() { return region; }
+
+    /**
+     * Return the speed for this level.
+     */
+    public float getSpeed() { return speed; }
+
+    /**
+     * Get the number of lanes for this level
+     */
+    public int getNumLanes() { return numLanes; }
+
+    /**
+     * Get the total number of blocks defined in this level file.
+     */
+    public int getTotalBlocks() { return totalBlocks; }
+
+    /**
+     * Get the number of blocks to select to build this level.
+     */
+    public int getUseBlocks() { return useBlocks; }
+
+    /**
+     * Get the padding between enemies for this level.
+     */
+    public float getPadding() { return padding; }
+
+    /**
+     * Return the songs for this level.
+     */
+    public ObjectMap<Genre,String> getSongs() { return songs; }
+
+    /**
+     * Return an array of enemies for this level.
+     */
+    public Array<Gnome> getGnomez() { return gnomez; }
+
+    /**
+     * Return the car for this level.
+     */
+    public Car getCar() { return yonda; }
+
+    /**
+     * Loads in a file to create a Level Object.
      *
-     * @param fileName name of JSON File with level information. */
-    public LevelObject(String fileName) {
+     * @param file name of JSON or Excel file with level information.
+     */
+    public LevelObject(String file) {
         yonda = new Car();
     }
 
     /**
-     * Creates a default three length road.
+     * Parse a JSON file for information about a level.
+     * @param file name of a JSON file
      */
-    public LevelObject() {
-        throw new java.lang.UnsupportedOperationException();
+    public void parseJSON(String file) {
+
     }
 
+    /**
+     * Parse an Excel file for information about a level.
+     * @param file name of an Excel file (xlsx or xls)
+     */
+    public void parseExcel(String file) throws Exception {
+        try {
+            FileInputStream f = new FileInputStream(new File("levels/" + file));
+
+            // Create a workbook and get the first sheet
+            Workbook wb = WorkbookFactory.create(f);
+            Sheet sh = wb.getSheetAt(0);
+
+            // Region information
+            String regionStr = sh.getRow(REGION_ROW).getCell(REGION_COL).getStringCellValue().toLowerCase();
+            if (regionStr.equals("the burbs"))
+                region = Region.SUBURBS;
+            else if (regionStr.equals("highway"))
+                region = Region.HIGHWAY;
+            else if (regionStr.equals("midwest"))
+                region = Region.MIDWEST;
+            else if (regionStr.equals("colorado"))
+                region = Region.COLORADO;
+            else
+                throw new RuntimeException("Not a valid region");
+
+            // Number of lanes - throws an IllegalStateException or NumberFormatException if error
+            numLanes = (int) sh.getRow(LANE_ROW).getCell(LANE_COL).getNumericCellValue();
+
+            // Iterate through cells for songs
+            for (int i = SONGS_START_ROW; i <= SONGS_END_ROW; i++) {
+                String songFile = sh.getRow(i).getCell(SONG_FILE_COL).getStringCellValue().toLowerCase();
+                String genreStr = sh.getRow(i).getCell(SONG_GENRE_COL).getStringCellValue();
+                if (genreStr.equals("pop"))
+                    songs.put(Genre.POP, songFile);
+                else if (genreStr.equals("creepy"))
+                    songs.put(Genre.CREEPY, songFile);
+                else if (genreStr.equals("dance"))
+                    songs.put(Genre.DANCE, songFile);
+                else if (genreStr.equals("action"))
+                    songs.put(Genre.ACTION, songFile);
+                else if (genreStr.equals("jazz"))
+                    songs.put(Genre.JAZZ, songFile);
+                else if (genreStr.equals("thug"))
+                    songs.put(Genre.THUG, songFile);
+                else if (genreStr.equals("comedy"))
+                    songs.put(Genre.COMEDY, songFile);
+                else
+                    throw new RuntimeException("Not a valid song genre");
+            }
+
+            // Iterate through the cells for road layout until "ROAD END" is reached
+            int roadCell = ROAD_START_ROW;
+            while (!sh.getRow(roadCell).getCell(0).getStringCellValue().toUpperCase().equals("ROAD END")) {
+                roadCell += 1;
+            }
+
+            // Close the file
+            f.close();
+
+        } catch (IOException e) {
+            throw new IOException("Error in closing the file");
+        } catch (InvalidFormatException e) {
+            throw new InvalidFormatException("Input file format invalid");
+        } catch (RuntimeException e) {
+            throw e;
+        }
+
+    }
 }
