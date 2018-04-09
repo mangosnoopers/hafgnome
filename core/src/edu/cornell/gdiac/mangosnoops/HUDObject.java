a9ecbfcf5d3d2eaf8b83c79d3aea0e0f302c755e
Susan Zou <swz6@cornell.edu>
2018-04-09 12:39:49 -0400

    more menu stuff in progress

--- core/src/edu/cornell/gdiac/mangosnoops/HUDObject.java
+++ core/src/edu/cornell/gdiac/mangosnoops/HUDObject.java
@@ -1,9 +1,5 @@
 package edu.cornell.gdiac.mangosnoops;
 
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector2;
-
 public class HUDObject {
 
 }
