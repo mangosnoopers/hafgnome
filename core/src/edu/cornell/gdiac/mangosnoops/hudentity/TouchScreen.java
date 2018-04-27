e3babcccfabefbd279fe5b134e8bfc7d9acae1ae
Susan Zou <swz6@cornell.edu>
2018-04-27 16:26:11 -0400

    make shaking static instead of individual

--- core/src/edu/cornell/gdiac/mangosnoops/hudentity/TouchScreen.java
+++ core/src/edu/cornell/gdiac/mangosnoops/hudentity/TouchScreen.java
@@ -3,11 +3,12 @@ package edu.cornell.gdiac.mangosnoops.hudentity;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import edu.cornell.gdiac.mangosnoops.GameCanvas;
+import edu.cornell.gdiac.mangosnoops.Image;
 
 public class TouchScreen {
 
-    Texture offScreen;
-    Texture dvdSlot;
+    Image offScreen;
+    Image dvdSlot;
     Texture buttonRadio;
     Texture buttonGps;
     Texture buttonDvd;
@@ -15,18 +16,18 @@ public class TouchScreen {
     GameCanvas c;
 
     public TouchScreen(Radio r, Texture os, Texture dvds) {
-        offScreen = os;
-        dvdSlot = dvds;
+        offScreen = new Image(0.83f, 0.24f, 0.26f, os, GameCanvas.TextureOrigin.MIDDLE);
+        dvdSlot = new Image(0.83f, 0.07f, 0.01f, dvds, GameCanvas.TextureOrigin.MIDDLE);
         radio = r;
     }
 
     public boolean inDvdSlot(Vector2 p) {
-        return c.inArea(p, dvdSlot, GameCanvas.TextureOrigin.MIDDLE, 0.83f, 0.07f, 0.2f, true);
+        return dvdSlot.inArea(p);
     }
 
     public void draw(GameCanvas canvas) {
         c = canvas;
-        canvas.draw(offScreen, GameCanvas.TextureOrigin.MIDDLE, 0.83f, 0.24f, 0.26f, true);
-        canvas.draw(dvdSlot, GameCanvas.TextureOrigin.MIDDLE, 0.83f, 0.07f, 0.2f, true);
+        offScreen.draw(canvas);
+        dvdSlot.draw(canvas);
     }
 }
