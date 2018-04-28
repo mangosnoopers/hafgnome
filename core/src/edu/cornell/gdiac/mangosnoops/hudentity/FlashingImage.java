15f894045fcce1a3d8037f5837ee044a16eb6272
tedbauer <tjb272@cornell.edu>
2018-04-28 00:22:01 -0400

    positioned circle things

--- core/src/edu/cornell/gdiac/mangosnoops/hudentity/FlashingImage.java
+++ core/src/edu/cornell/gdiac/mangosnoops/hudentity/FlashingImage.java
@@ -1,7 +1,52 @@
 package edu.cornell.gdiac.mangosnoops.hudentity;
 
+import com.badlogic.gdx.graphics.Texture;
+import edu.cornell.gdiac.mangosnoops.GameCanvas;
 import edu.cornell.gdiac.mangosnoops.Image;
 
+/**
+ * FlashingImage flashes on the screen at a particular rate. Its
+ * visibility can be toggled. For use in the Tutorial implementation.
+ */
 public class FlashingImage extends Image {
-    
+
+    /** How quickly the image flashes */
+    private float FLASHING_RATE = 5f;
+
+    /**
+     * Whether or not the Image is visible to the user.
+     *  Not to be confused with hidden:
+     *   - when isVisibleToUser is true, the image will
+     *     flash on and off.
+     *   - when isVisibleToUser is false, the image will
+     *      remain invisible.
+     */
+    private boolean isVisibleToUser;
+
+    /**
+     * Whether or not the image is hidden - this toggles on
+     * and off repeatedly
+     */
+    private boolean hidden;
+
+    public FlashingImage(float x, float y, float relSca, Texture tex) {
+        super(x, y, relSca, tex);
+        isVisibleToUser = true;
+    }
+
+    /**
+     * Set the visibility to inVis.
+     * @param isVis visibility
+     */
+    public void setVisible(boolean isVis) {
+        isVisibleToUser = isVis;
+    }
+
+    public void update(float delta) {
+
+    }
+
+    public void draw(GameCanvas canvas) {
+        if (isVisibleToUser && !hidden) super.draw(canvas);
+    }
 }
