88cdbff4451d6c5c8dca05dc42a2587100354b4d
stsinwell <sas575@cornell.edu>
2018-05-07 20:41:31 -0400

    billboards but need to fix drawing

--- core/src/edu/cornell/gdiac/mangosnoops/NormalLevelController.java
+++ core/src/edu/cornell/gdiac/mangosnoops/NormalLevelController.java
@@ -6,14 +6,9 @@ import com.badlogic.gdx.utils.Array;
 
 public class NormalLevelController extends GameplayController {
 
-    /** Files */
-    private static final String BILLBOARD_END_IS_NEAR = "images/billboards/the_end_is_near.png";
-
-    /** Texture types */
-    private Texture billboardEndIsNearTexture;
-
     public NormalLevelController(GameCanvas canvas, LevelObject level, SoundController sc) {
-        super(canvas, level.getLevelEndY(), level.getEnemiez(), level.getEvents(), level.getSongs(), sc);
+        super(canvas, level.getLevelEndY(), level.getEnemiez(), level.getEvents(),
+                level.getSongs(), sc, level.getRoadsideObjs());
     }
 
     public void start(float x, float y) {
@@ -26,13 +21,10 @@ public class NormalLevelController extends GameplayController {
 
     public void preLoadContent(AssetManager manager, Array<String> assets) {
         super.preLoadContent(manager, assets);
-        manager.load(BILLBOARD_END_IS_NEAR, Texture.class);
-        assets.add(BILLBOARD_END_IS_NEAR);
     }
 
     public void loadContent(AssetManager manager) {
         super.loadContent(manager);
-        billboardEndIsNearTexture = createTexture(manager, BILLBOARD_END_IS_NEAR);
     }
 
 }
