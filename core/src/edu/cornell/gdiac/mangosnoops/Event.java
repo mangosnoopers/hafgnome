ca97beebcbbd61af4a6f450cdeb5563e1ba45678
stsinwell <sas575@cornell.edu>
2018-05-18 02:19:27 -0400

    child requesting music events

--- core/src/edu/cornell/gdiac/mangosnoops/Event.java
+++ core/src/edu/cornell/gdiac/mangosnoops/Event.java
@@ -8,7 +8,8 @@ public class Event {
 
     /** Enumeration of different types of events that can occur */
     public enum EventType {
-        REAR_ENEMY, SUN_START, SUN_END, NED_WAKES_UP, NOSH_WAKES_UP, SAT_QUESTION
+        REAR_ENEMY, SUN_START, SUN_END, NED_WAKES_UP, NOSH_WAKES_UP, SAT_QUESTION,
+        NED_REQUESTS_MUSIC, NOSH_REQUESTS_MUSIC
     }
 
     /** Return the y-coordinate where this event occurs */
